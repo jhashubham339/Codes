f08c67860abe02f847c55b33b47f1c0630da8743
Shubham kumar jha <Shubham Kumar Jha>
2023-06-27 20:22:45 +0530

    First commit

---
+++ JAVA/src/exceptionHandling/checkedExceptions.java
@@ -0,0 +1,17 @@
+//Also called Compile time Exception
+package exceptionHandling;
+
+//import java.io.FileInputStream;
+
+public class checkedExceptions {
+
+	public static void main(String[] args) {
+		
+		//FileInputStream fis = new FileInputStream("d:/abc.text");
+       /*Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+		Unhandled exception type FileNotFoundException
+		at exceptionHandling.checkedExceptions.main(checkedExceptions.java:10)
+		*/
+	}
+
+}
