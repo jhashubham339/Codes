17102004c19b4925ab8c32d26dc8cbf2ea9dab65
Shubham kumar jha <Shubham Kumar Jha>
2023-09-19 18:48:48 +0530

    lambda and generics

---
+++ JAVA/src/lambdaExpression/functionalInterface.java
@@ -0,0 +1,14 @@
+package lambdaExpression;
+
+@FunctionalInterface
+public interface functionalInterface {
+	 public abstract void sayHello(); 
+		// public abstract void hello();
+	 
+	 // How to use Functional Interface
+	 
+	 // 1. create separate class and implement this interface
+	 // 2. anonymous class for implementing interface
+	 // 3. lambda expression
+
+}
